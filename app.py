7dbb89cd2f4141be0ea683a9df6e691900b94ba6
ADITHYA ABHIRAM CHITTIMALLA <adithya.abhiram.2805@gmail.com>
2026-05-26 14:12:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ if uploaded_file is not None:
     # Prediction
     processed = preprocess_image(image)
 
-    prediction = model.predict(processed, verbose=0)
+    prediction = model.predict(processed)
 
     predicted_index = np.argmax(prediction)
 
